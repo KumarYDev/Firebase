09df7b72ec0aaffe59a21eee2f218dc78aad828d
Windows Kumar <hassain.r06@gmail.com>
2017-12-19 15:31:59 -0500

    Initial commit

---
+++ app/src/main/java/com/usermindarchive/h/firebase/MainActivity.java
@@ -0,0 +1,29 @@
+package com.usermindarchive.h.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+@BindView(R.id.get)
+    Button send;
+@BindView(R.id.show)
+    EditText show;
+firebase fire;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fire=new firebase();
+    }
+
+    @OnClick(R.id.get)
+    public void send(){
+        fire.sendData();
+
+    }
+}
---
+++ app/src/main/java/com/usermindarchive/h/firebase/firebase.java
@@ -0,0 +1,16 @@
+package com.usermindarchive.h.firebase;
+
+/**
+ * Created by HERO on 12/11/2017.
+ */
+
+public class firebase {
+
+    public firebase(){
+
+    }
+    public void sendData(){
+
+
+    }
+}
